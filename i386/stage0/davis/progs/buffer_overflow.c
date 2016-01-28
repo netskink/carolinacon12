132e4f30374ef23a7bcada2f6f9fe3c6ed8611f4
John F. Davis <davisjf@gmail.com>
2016-01-28 15:22:07 -0500

    buffer overflow for i386 works

---
+++ i386/stage0/davis/progs/buffer_overflow.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void nevercalled(void) {
+	printf("I should never be called.\n");
+	exit(0);
+}
+
+void vulnerable(char *arg) {
+	char buffer[10];
+	strcpy(buffer,arg);
+}
+
+int main(int argc, char **argv) {
+
+	vulnerable(argv[1]);
+	printf("argv[1] = %s\n", argv[1]);
+
+	return(0);
+}
+
+
